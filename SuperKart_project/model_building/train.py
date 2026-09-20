9258388d14bfe6dd03e527c6b966e64424d20c48
RanjuKargwal <89624812+RanjuKargwal@users.noreply.github.com>
2026-09-20 23:28:32 +0530

    update SuperKart_project/model_building/train.py

--- SuperKart_project/model_building/train.py
+++ SuperKart_project/model_building/train.py
@@ -5,7 +5,8 @@ import mlflow
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import make_column_transformer
-
+from sklearn.pipeline import make_pipeline
+import xgboost as xgb
 
 # Set your auth token here (replace with your actual token from the ngrok dashboard)
 NGROK_AUTH_TOKEN = '3JaaoLCSxHpuLoz0LTvkUetOyfX_2jMkMZCB4kKCAnzgauBaz'
